f857d7b84ba5603708db036a509bd3ca9141f0ca
Tomas Petricek <tomas@tomasp.net>
2015-09-12 04:04:50 +0100

    Recover F5 code (but doesnt seem to work)

--- app.fsx
+++ app.fsx
@@ -82,3 +82,21 @@ let app =
       pathScan "/tags/%s" Tag.showSnippets
       ( path "/rss/" <|> path "/rss" <|> path "/pages/Rss" <|> path "/pages/Rss/" ) >>= Rss.getRss
       browseStaticFiles ]
+
+// -------------------------------------------------------------------------------------------------
+// To run the web site, you can use `build.sh` or `build.cmd` script, which is nice because it
+// automatically reloads the script when it changes. But for debugging, you can also use run or
+// run with debugger in VS or XS. This runs the code below.
+// -------------------------------------------------------------------------------------------------
+
+#if INTERACTIVE
+#else
+let cfg =
+  { defaultConfig with
+      bindings = [ HttpBinding.mk' HTTP  "127.0.0.1" 8011 ]
+      homeFolder = Some __SOURCE_DIRECTORY__ }
+let _, server = startWebServerAsync cfg app
+Async.Start(server)
+System.Diagnostics.Process.Start("http://localhost:8011")
+System.Console.ReadLine() |> ignore
+#endif
