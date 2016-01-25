23163957b349efaf931cc8342b4f3f38c6e1b729
Tomas Petricek <tomas@tomasp.net>
2016-01-25 01:07:30 +0000

    Catch IE exception in copy function (#9)

--- web/content/copycontent.js
+++ web/content/copycontent.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
     var range, selection;
 
     if (doc.body.createTextRange) {
+      try
+      {        
         range = document.body.createTextRange();
         range.moveToElementText(element);
         range.select();
+      } catch(e) { }
     } else if (window.getSelection) {
         selection = window.getSelection();
         range = document.createRange();
@@ -27,19 +30,18 @@ $(document).ready(function () {
     if (action == 'show-link') {
       dialog.find('.modal-dialog').removeClass('modal-lg')
       dialog.find('.modal-title').text('Link to Snippet');
-      dialog.find('.modal-body')
-        .html('<p>' + $(location).attr('href') + '</p>')
-        .selectText();
+      dialog.find('.modal-body-inner').html('<pre id="selectMe">' + $(location).attr('href') + '</pre>');
+      $("#selectMe").selectText();
     } else if (action == 'show-source') {
       dialog.find('.modal-dialog').addClass('modal-lg');
       dialog.find('.modal-title').text('Snippet Source');
       var snippetSourceUrl = document.location.href.replace(document.location.pathname, '/raw/') + snippetId;
       $.get(snippetSourceUrl, function (data) {
-        dialog.find('.modal-body')
-          .html('<pre>'+ data +'</pre>')
+        dialog.find('.modal-body-inner')
+          .html('<div id="selectMe"><pre>'+ data +'</pre></div>')
           .css('max-height', $(window).height() * 0.7)
-          .css('overflow-y', 'auto')
-          .selectText();
+          .css('overflow-y', 'auto');
+        $("#selectMe").selectText();
       });
     }
 
