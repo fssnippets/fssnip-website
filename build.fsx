34fef5c7a39d412fdc5b819f49fe8c49883141d4
Tomas Petricek <tomas@tomasp.net>
2023-09-08 11:35:22 +0200

    Remove the use of private npm in minify step

--- build.fsx
+++ build.fsx
@@ -31,19 +31,19 @@ Target.create "minify" (fun _ ->
   Trace.trace "Node js web compilation thing"
 
   // Use nuget tools if windows, require already installed otherwise
-  let npmPath = Path.GetFullPath "packages/jstools/Npm.js/tools"
-  let nodePath = Path.GetFullPath "packages/jstools/Node.js"
-
-  if Environment.isWindows then
-    [ npmPath ; nodePath ; Environment.environVar "PATH" ]
-    |> String.concat ";"
-    |> Environment.setEnvironVar "PATH"
-
-  let getNpmFilePath (p : Npm.NpmParams) = 
-    if Environment.isWindows then Path.Combine(npmPath, "npm.cmd") else p.NpmFilePath
-
-  Npm.install (fun p -> { p with NpmFilePath = getNpmFilePath p })
-  Npm.exec "run-script build" (fun p -> { p with NpmFilePath = getNpmFilePath p })
+  //let npmPath = Path.GetFullPath "packages/jstools/Npm.js/tools"
+  //let nodePath = Path.GetFullPath "packages/jstools/Node.js"
+//
+  //if Environment.isWindows then
+  //  [ npmPath ; nodePath ; Environment.environVar "PATH" ]
+  //  |> String.concat ";"
+  //  |> Environment.setEnvironVar "PATH"
+//
+  //let getNpmFilePath (p : Npm.NpmParams) = 
+  //  if Environment.isWindows then Path.Combine(npmPath, "npm.cmd") else p.NpmFilePath
+//
+  Npm.install (fun p -> p) //{ p with NpmFilePath = getNpmFilePath p })
+  Npm.exec "run-script build" (fun p -> p) //{ p with NpmFilePath = getNpmFilePath p })
 )
 
 Target.create "clean" (fun _ ->
