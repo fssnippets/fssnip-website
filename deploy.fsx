4b810531dd259d6ceb5b00167429197dcc97b1e5
Peter Dubec <peter@compositional-it.com>
2023-10-26 02:05:34 +0100

    Update project for hosting by Compositional-IT

---
+++ deploy.fsx
@@ -0,0 +1,63 @@
+#r "nuget: Farmer"
+
+open System
+open Farmer
+open Farmer.Arm
+open Farmer.Builders
+
+let appName = "fs-snip"
+    
+let trimmedAppName = appName.Replace("-", "")
+let storageAccountName = $"{trimmedAppName}storage"
+let artifactDir = "wwwroot"
+let blobContainerName = "data"
+let recaptchaSecret = Environment.GetEnvironmentVariable "RECAPTCHA_SECRET"
+
+let logAnalytics =
+    logAnalytics {
+        name $"{appName}-workspace"
+    }
+
+let insights =
+    appInsights {
+        name $"{appName}-ai"
+        log_analytics_workspace logAnalytics
+    }
+
+let storage = storageAccount {
+    name storageAccountName
+    sku Storage.Sku.Standard_LRS
+    add_private_container blobContainerName
+}
+
+let app = webApp {
+    name appName 
+    runtime_stack Runtime.DotNetCore31
+    link_to_app_insights insights
+    sku WebApp.Sku.S1
+    operating_system OS.Linux
+    connection_string ("FSSNIP_STORAGE", storage.Key)
+    settings [
+        "FSSNIP_HOME_DIR", "."
+        "RECAPTCHA_SECRET", recaptchaSecret
+    ]
+    zip_deploy artifactDir
+    https_only
+    always_on
+}
+
+let deployment = arm {
+    location Location.WestEurope
+    add_resources [
+        logAnalytics
+        insights
+        app
+        storage
+    ]
+    output "storage-key" storage.Key
+}
+
+deployment
+|> Deploy.execute appName Deploy.NoParameters
+|> Map.find "storage-key"
+|> fun key -> IO.File.WriteAllText("deployed_storage_key.txt", key)
---
+++ fssnip.ps1
@@ -0,0 +1,44 @@
+param($command='run')
+
+Write-Output "Executing command: $command"
+
+$env:PAKET_SKIP_RESTORE_TARGETS="true"
+$env:IP_ADDRESS="0.0.0.0"
+$env:FSSNIP_HOME_DIR="."
+$env:RECAPTCHA_SECRET=<insert_value_here>
+$env:fssnip_storage_key=<insert_value_here>
+$env:fssnip_data_url="https://github.com/fssnippets/fssnip-data/archive/master.zip"
+
+function Restore-GlobalJson { if (Test-Path -Path './notglobal.json') { Rename-Item './notglobal.json' -NewName './global.json' } }
+function Hide-GlobalJson { if (Test-Path -Path './global.json') { Rename-Item './global.json' -NewName './notglobal.json' } }
+
+function Build-App {
+    # in case another script left the global.json renamed
+    Restore-GlobalJson
+    dotnet tool restore
+    dotnet paket restore
+    dotnet fake run build.fsx
+}
+
+function Start-App {
+    # in case another script left the global.json renamed
+    Restore-GlobalJson
+    dotnet wwwroot/fssnip.dll
+}
+
+function Deploy-App {
+    Hide-GlobalJson
+    dotnet fsi deploy.fsx
+    Restore-GlobalJson
+}
+
+function Deploy-Data {
+    Hide-GlobalJson
+    dotnet fsi upload-blobs.fsx
+    Restore-GlobalJson
+}
+
+if ($command -eq 'run') { Start-App }
+if ($command -eq 'build') { Build-App }
+if ($command -eq 'deploy') { Deploy-App }
+if ($command -eq 'upload') { Deploy-Data }
